8e4a6180b8fbb049ca599300727e7ca44ff04e2c
Marc Ford <mrfxyz567@gmail.com>
2020-08-06 10:53:36 -0400

    Add more celery utilities

--- django_handy_utils/celery.py
+++ django_handy_utils/celery.py
@@ -1,9 +1,33 @@
+import typing
 import os
 
 
-def detect_periodic_tasks(project_root):
+def detect_tasks(root_dirpath: str) -> tuple:
+    """
+    Detect Celery tasks from a root directory
+    """
+    tasks = []
+    file_path = os.path.join(root_dirpath)
+    for root, dirs, files in os.walk(file_path):
+        for filename in files:
+            if os.path.basename(root) == "tasks":
+                if filename != "__init__.py" and filename.endswith(".py"):
+                    task = (
+                        os.path.join(root, filename)
+                        .replace(root_dirpath + "/", "")
+                        .replace("/", ".")
+                        .replace(".py", "")
+                    )
+                    tasks.append(task)
+    return tuple(tasks)
+
+
+def detect_periodic_tasks(root_dirpath: str) -> typing.Dict:
+    """
+    Detect Celery periodic tasks from a root directory
+    """
     tasks = {}
-    file_path = os.path.join(project_root)
+    file_path = os.path.join(root_dirpath)
     for root, dirs, files in os.walk(file_path):
         for filename in files:
             if os.path.basename(root) == "tasks":
@@ -16,3 +40,19 @@ def detect_periodic_tasks(project_root):
                     except NameError:
                         pass
     return tasks
+
+
+class RetriableTaskException(Exception):
+    """
+    Exception for celery tasks that indicates task can be retried
+    """
+
+    pass
+
+
+class UnRetriableTaskException(Exception):
+    """
+    Exception for celery tasks that indicates task cannot be retried
+    """
+
+    pass
